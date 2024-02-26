745e8d3f9006ea9f79fe73ea1dd462b29239a95d
ankitasengupta474 <121786008+ankitasengupta474@users.noreply.github.com>
2024-02-26 15:47:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,request,jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.get("/")
+
+def index_get():
+    return render_template("index.html")
+                           
+@app.post("/predict")
+           
+def predic():
+    text =request.get_json().get('message')                               
+    # TODO: check if text is valid
+    response =get_response(text)
+    message ={"answer": response}
+    return jsonify(message) 
+if  __name__ == '__main__':
+#   app.run(debug =True)                       
+    app.run(host='103.159.85.126', port=5002)
